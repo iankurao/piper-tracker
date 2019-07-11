38965e89d2508f9e0ace197b7889c649cd3496e0
iankurao <kuraoian@gmail.com>
2019-07-11 08:40:05 +0300

    configure authentication url patterns

--- pricetracker/urls.py
+++ pricetracker/urls.py
@@ -13,9 +13,34 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from users import views as user_views
+
+from django.conf import settings
+from django.conf.urls.static import static
+from datetime import datetime
+
+# from background.views import hello,kya
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('bgt/',include('background.background_urls')),
+   
+    path('',include('track.trackurls')),
+    path('register/', user_views.register,name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('profile/', user_views.profile,name='profile'),
+     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+=staticfiles_urlpatterns()
